122b9436c1456f7bfd10eab462b7dfb173a5058f
christinaaa77 <114158922+christinaaa77@users.noreply.github.com>
2024-11-08 23:09:35 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './store';  // Pastikan store di-import
-import App from './App';  // Pastikan App di-import
+import store from './store';  
+import App from './App';  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
